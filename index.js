16134ce14cb7163409be4f1544713d5e12469cfe
suhailgupta03 <suhailgupta03@gmail.com>
2017-01-09 08:34:38 +0530

    added apis

--- index.js
+++ index.js
@@ -1,27 +1,91 @@
 var FileParser = require('./lib/parser/FileParser');
 var MPipe = require('./lib/db/MPipe');
 
-var map = FileParser.parseFile();
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
 
-if(map) {
-    MPipe.openConnection((err,res) => {
+MPipe.openConnection((err,res)=>{
+   if(err)
+       console.log('Failed to open connection to MySQL');
+    else
+        console.log(res);
+});
+
+var server = app.listen(8081,()=>{
+                console.log(`Server started at port ${server.address().port}`);
+            });
+
+
+app.get('/',function(req,res,next){
+   res.sendFile('index.html',(err)=>{
+       if(err)
+           console.log('Failed to complete the get request');
+   }) 
+});
+
+app.get('/data/all',(req,res,next)=>{
+    var storedProcedure = "call get_product_info(null)";
+    MPipe.query(storedProcedure,(err,row,fields)=>{
+        var responseObject = {};
+       if(err)
+           responseObject['error'] = "Failed to fetch data";
+        else
+            responseObject['data'] = row[0]; 
+        res.json(responseObject)
+            .end();
+    });
+});
+
+app.param('id',function(req, res, next, id){
+    req.id = id; 
+    next();
+});
+
+app.get('/instrument/:id',(req,res,next)=>{
+   var storedProcedure = `call get_product_info(${req.id})`;
+    MPipe.query(storedProcedure,(err,row,fields)=>{
+       var response = '';
         if(err)
-            throw err;
-        if(res){
-            var numberOfRowsInserted = 0;
-            var numberOfRowsFailedToInsert = 0;
-            for(var id in map){
-                var position = map[id]['position'];
-                var status = map[id]['status'];
-                var callToStoredProcedure = `call insert_product_info(${id},${position},${status})`;
-                MPipe.query(callToStoredProcedure, (err,rows,fields) => {
-                    if(err)
-                        console.log(`${++numberOfRowsFailedToInsert} rows(s) failed to insert, ${err}`);
-                    else
-                        console.log(`${++numberOfRowsInserted} row(s) inserted`);
-                });
+            response.status = 404;
+        else{
+            if(row[0].length == 0){
+                // Id not found
+                response = 404;
+            }else{
+                response = row[0][0]['status'];
             }
         }
-             
+            
+        res.json(response)
+        .end();
     });
-}
+});
+
+app.post('/addItem/',(req,res,next)=>{
+    
+});
+//var map = FileParser.parseFile();
+//
+//if(map) {
+//    MPipe.openConnection((err,res) => {
+//        if(err)
+//            throw err;
+//        if(res){
+//            var numberOfRowsInserted = 0;
+//            var numberOfRowsFailedToInsert = 0;
+//            for(var id in map){
+//                var position = map[id]['position'];
+//                var status = map[id]['status'];
+//                var callToStoredProcedure = `call insert_product_info(${id},${position},${status})`;
+//                MPipe.query(callToStoredProcedure, (err,rows,fields) => {
+//                    if(err)
+//                        console.log(`${++numberOfRowsFailedToInsert} rows(s) failed to insert, ${err}`);
+//                    else
+//                        console.log(`${++numberOfRowsInserted} row(s) inserted`);
+//                });
+//            }
+//        }
+//             
+//    });
+//}
---
+++ public/js/app.js
@@ -0,0 +1,96 @@
+/*global angular */
+(function () {
+    'use strict';
+    angular.module('filllist', ['ngRoute'])
+        .config(function ($routeProvider, $locationProvider) {
+            $routeProvider
+                .when("/", {
+                    templateUrl : "templates/home.html",
+                    controller: 'homeCtrl'
+                })
+                .when("/showAll", {
+                    templateUrl : "templates/all.html",
+                    controller: 'showAllCtrl'
+                })
+                .when("/add", {
+                    templateUrl : "templates/add.html",
+                    controller: 'addItemCtrl'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+        })
+        .service('dataService', ['$http', '$interval', function ($http, $interval) {
+            var autoRefreshTimer;
+            return {
+                getAllData: function () {
+                    return $http.get('/data/all', {}).then(function (response) {
+                        return response.data;
+                    }, function (error) {
+                        
+                    });
+                },
+                getInstrumentData: function (instrumentId) {
+                    return $http.get('/instrument/' + instrumentId, {}).then(function (response) {
+                        return response.data;
+                    }, function (error) {
+                        
+                    });
+                },
+                addItem: function (inputData) {
+                    return $http.post('/addItem/', {data: inputData}, {}).then(function(response){
+                        return response.data;
+                    }, function(error){
+                        
+                    })
+                },
+                autoRefreshData: function (callback) {
+                    $http.get('/data/all', {}).then(function (response) {
+                        if (callback && typeof callback === "function") {
+                                callback(response.data);
+                            }
+                        }, function (error) {
+
+                    });
+                    autoRefreshTimer = $interval(function () {
+                        $http.get('/data/all', {}).then(function (response) {
+                            if (callback && typeof callback === "function") {
+                                callback(response.data);
+                            }
+                        }, function (error) {
+
+                        });
+                    }, 5000);
+                }
+            };
+
+        }])
+        .controller('homeCtrl', ['$scope', 'dataService', function ($scope, dataService) {
+            dataService.getAllData().then(function (response) {
+                $scope.itemlist = response.data;
+            });
+            $scope.searchProductId = '';
+            $scope.searchProductStatus = '';
+            $scope.getProductStatus = function () {
+                dataService.getInstrumentData($scope.searchProductId).then(function(response){
+                    if(response != 404) 
+                        $scope.searchProductStatus = response;
+                    else {
+                        $scope.searchProductStatus = 'Poduct Not Found';
+                    }
+                });
+            }
+            
+        }])
+        .controller('addCtrl', ['$scope', 'dataService', function ($scope, dataService) {
+            $scope.message = '';
+            dataService.addItem(function (response) {
+                $scope.message = 'Item added successfully';
+            });
+        }])
+        .controller('showAllCtrl', ['$scope', 'dataService', function ($scope, dataService) {
+            dataService.autoRefreshData(function (response) {
+                $scope.itemlist = response.data;
+            });
+        }]);
+}());
